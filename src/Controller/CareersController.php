d55907ce2cdcdf8c9c61c86c85360e195f27f6a2
Mogsu <miguelosinada@gmail.com>
2026-05-18 09:58:33 +0800

    feat: added job posters, updated UI for view job details

---
+++ src/Controller/CareersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CareersController extends AbstractController
+{
+    private function renderWithDefaults(string $view, array $params = []): Response
+    {
+        return $this->render($view, array_merge($params, [
+            'controller_name' => 'CareersController',
+        ]));
+    }
+
+    #[Route('/job-posting', name: 'app_careers')]
+    public function index(): Response
+    {
+        return $this->renderWithDefaults('Careers/index.html.twig');
+    }
+}
