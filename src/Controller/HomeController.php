239a083fd0ff21433a9f1bf66387f141d6720360
Mogsu <miguelosinada@gmail.com>
2026-06-03 16:47:40 +0800

    refactor: changed home sections and added new pages
    + Updated carousel cards
    + Updated Why FEU Section
    + Updated Events Section
    + Added new pages for Terms and Privacy, along with footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,4 +21,16 @@ final class HomeController extends AbstractController
     {
         return $this->renderWithDefaults('home/index.html.twig');
     }
+
+    #[Route('/terms-and-conditions', name: 'app_terms_and_conditions')]
+    public function termsAndConditions(): Response
+    {
+        return $this->renderWithDefaults('legal/terms_and_conditions.html.twig');
+    }
+
+    #[Route('/privacy-policy', name: 'app_privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->renderWithDefaults('legal/privacy_policy.html.twig');
+    }
 }
