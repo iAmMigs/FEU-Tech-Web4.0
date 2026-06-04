a655ee721b0ec0bf065660cf4a3842c4fea6cbc3
JOYBERCE <bercenioj@gmail.com>
2026-06-04 13:23:37 +0800

    Update for deployment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,11 +22,31 @@ final class HomeController extends AbstractController
         return $this->renderWithDefaults('home/index.html.twig');
     }
 
+    
+            #[Route('/features/shaping-the-future-of-education-utah-valley-university-feu-tech-unite-for-ai-innovation-workshop', name: 'event_one')]
+            public function event_one(): Response
+            {
+                return $this->renderWithDefaults('home/events/event_one.html.twig');
+            }
+
+            #[Route('/features/taranasapiyu-2026-welcoming-the-next-generation-of-innovators-at-feu-tech', name: 'event_two')]
+            public function event_two(): Response
+            {
+                return $this->renderWithDefaults('home/events/event_two.html.twig');
+            }
+
+            #[Route('/features/ftic-ramps-up-the-culture-of-innovation-at-feu-tech-with-a-campus-wide-symposium', name: 'event_three')]
+            public function event_three(): Response
+            {
+                return $this->renderWithDefaults('home/events/event_three.html.twig');
+            }
+
     #[Route('/terms-and-conditions', name: 'app_terms_and_conditions')]
     public function termsAndConditions(): Response
     {
         return $this->renderWithDefaults('legal/terms_and_conditions.html.twig');
     }
+    
 
     #[Route('/privacy-policy', name: 'app_privacy_policy')]
     public function privacyPolicy(): Response
